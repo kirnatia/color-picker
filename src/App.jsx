e3ebabd37126e4ba76d6212413802eac787d36a8
kirnatia <natiakiria@gmail.com>
2023-07-18 20:57:01 -0400

    add colors, edit css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-// Write your Color component here
+import { useState } from 'react';
+
+const Color = ({ color, onSelect }) => {
+  const onClick = () => {
+    onSelect(color)
+    
+  };
+
+  return <div className={color} onClick={onClick}></div>;
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState('');
+
+  const handleColorSelect = (color) => {
+    setSelectedColor(color);
+  };
+
   return (
     <div id="container">
       <div id="navbar">
-        <h1>hello</h1>
-        <div>Currently selected: </div>
-        <div className="red">red</div>
+        <h1>Hello</h1>
+        <div>Currently selected: {selectedColor}</div>
+        <div className={selectedColor}></div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+
+      <div id="colors-list">
+      
+      <Color color="red" onSelect={handleColorSelect} />
+        <Color color="yellow" onSelect={handleColorSelect} />
+        <Color color="blue" onSelect={handleColorSelect} />
+        <Color color="green" onSelect={handleColorSelect} />
+        <Color color="violet" onSelect={handleColorSelect} />
+        
+        <Color color="pink" onSelect={handleColorSelect} />
+      </div>
     </div>
   );
 };
